07d4bbeda7953d2a0633362c65d2333a8e7e4f63
Branden Coker <branden.coker@gmail.com>
2018-04-13 23:08:40 -0500

    scaffold system architecture

---
+++ MSHPLogger.Tests/MSHPLoggerRepositoryIntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MSHPLogger.Tests
+{
+    [TestFixture]
+    public class MSHPLoggerRepositoryIntegrationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+
+        [Test]
+        public void TestMethod1()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+    }
+}
---
+++ MSHPLogger.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSHPLogger.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("MSHPLogger.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06132cbd-5bc5-4282-8226-09d456c3a1c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
