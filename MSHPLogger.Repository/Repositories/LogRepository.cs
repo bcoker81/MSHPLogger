df84a6f825a33963eb4fc3a3477a23cbd8bbe1f6
Branden Coker <branden.coker@gmail.com>
2018-04-13 23:59:14 -0500

    add code to repo

--- MSHPLogger.Repository/Repositories/LogRepository.cs
+++ MSHPLogger.Repository/Repositories/LogRepository.cs
@@ -10,6 +10,18 @@ namespace MSHPLogger.Repository.Repositories
 {
     public class LogRepository : ILogger
     {
+        private LoggerContext _db;
+
+        public LogRepository(LoggerContext db)
+        {
+            _db = db;
+        }
+
+        public LogRepository()
+        {
+            _db = new LoggerContext();
+        }
+
         public bool LogError(LogEntry log)
         {
             throw new NotImplementedException();
